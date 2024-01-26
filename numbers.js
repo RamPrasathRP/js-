8c44e2f38dab8061f1a7229269c025f844cf5bff
RamPrasathRP <125652951+RamPrasathRP@users.noreply.github.com>
2024-01-27 03:33:59 +0530

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,28 @@
+let string = ("RP" + "0");
+console.log(string);
+//ipo itha print pannumbothu output namake..RP0...IPDI varum....because "" quotes kulla charaster kuduthalum ...number kuduthalum athu string ah tha eduthukum...
+let alpha =("1"+"1");
+console.log(alpha);
+//itha print panna namaku answer 11 nu tha varum becox..values ah string ah kuduthurukom...
+//but namma antha numbers ah add pannanum na ...
+let beta = (1+1);
+console.log(beta);
+//ipo ithoda output namaku 2 nu varum
+let gamma =("1"+1);
+console.log(gamma);
+//itha print panna namaku answer 11 nu tha varum bcoz it contains string and number.
+let albega = (1+"1");
+console.log(albega);
+//itha print panna namaku answer 11 nu tha varum bcoz it contains string and number.
+let phoneNum = (6379610219);
+            // na en number ah update panna ..puthu variable create panna thevailla...i can update as..
+phoneNum =(9043834050);
+console.log(phoneNum);
+// ithuku output update panna value tha varum...
+//const doorNo=(25/15);   ithuku typeerror varum..bcoz const..constant oda value can't be changed ..
+//doorNo=(55/123); ithuku typeerror varum..bcoz const..constant oda value can't be changed ..
+//console.log("doorNo"); ithuku typeerror varum..bcoz const..constant oda value can't be changed ..
+let Score=(100);
+const Bonus=(50);
+let finalScore =(Score+Bonus);
+console.log(finalScore);
---
+++ variables.js
@@ -0,0 +1,22 @@
+
+//"let" is used for variable declaration da thambi..
+//here "purse" is represented as variable name da thambudu....
+//double quotes "" kulla kudukuratha string nu solluvom da ....
+//ithula variable declare panni..athuku name purse nu kuduthu.purse la money nu oru string kuduthurukom
+let purse = "money";
+//printing the variable item using variable name
+console.log (purse);
+//ipo namma variable la number or integer store pannanum na ....namma " " quotes kudukama podanum da thambi...
+
+let number = 31082003;
+let firstName = "Ram Prasath";
+//console.log("firstName + lastName");
+let lastName = "RP";
+//ipdi kuduka koodathu bcoz "" quotes kulla varum pothu athu variables are act aagathu.."quotes kulla declare tha pannamudium"
+console.log(firstName + lastName);
+//variable let illa maium create pannalam..athuku const keyword use pannuvom
+//dif b/w let and const ..
+//=> let oda values ah namma variable name vachu update panna mudium..but
+//=> const oda value va update pannamudiyathu ..once it declared it is fixed.....
+const name =('RamPrasath');
+//name= ramu;//ipdi kudutha error tha varum
